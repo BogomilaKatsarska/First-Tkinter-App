85e07919f38b8edd32deaae7df863edf85e6fb7d
Bogomila <bkatsarska@abv.bg>
2024-02-09 18:47:27 +0200

    added labels

---
+++ 04_labels.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+root = tk.Tk()
+root.geometry("600x400")
+root.resizable(False, False)
+root.title("Widget Examples")
+
+label = ttk.Label(root, text="Hello, world!", padding=20)
+label.config(font=("Segoe UI", 20))
+label.pack()
+
+image = Image.open("Python.svg.png").resize((64, 64))
+photo = ImageTk.PhotoImage(image)
+label2 = ttk.Label(root, text="Image with text", image=photo, padding=5, compound="right")
+label2.pack()
+
+
+
+root.mainloop()
